6a8e36a68782b9181ee6e8e11c1e4558d01178a4
stack72 <public@paulstack.co.uk>
2020-04-27 18:27:37 +0100

    Regenerate datasource examples to be async

--- sdk/nodejs/getPublicKey.ts
+++ sdk/nodejs/getPublicKey.ts
@@ -19,9 +19,9 @@ import * as utilities from "./utilities";
  * import * as fs from "fs";
  * import * as tls from "@pulumi/tls";
  * 
- * const example = tls.getPublicKey({
+ * const example = pulumi.output(tls.getPublicKey({
  *     privateKeyPem: fs.readFileSync("~/.ssh/id_rsa", "utf-8"),
- * });
+ * }, { async: true }));
  * ```
  *
  * > This content is derived from https://github.com/terraform-providers/terraform-provider-tls/blob/master/website/docs/d/public_key.html.md.
