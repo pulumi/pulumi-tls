776b40b25f039bba938b8a91bf99b514c748c572
Anton Tayanovskyy <anton@pulumi.com>
2023-01-20 11:13:19 -0500

    Update upstream to pre-release afdd54107aba version on PF 1.1.1

--- provider/shim/shim.go
+++ provider/shim/shim.go
@@ -1,11 +1,10 @@
 package shim
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/terraform-providers/terraform-provider-tls/internal/provider"
+	framework "github.com/hashicorp/terraform-plugin-framework/provider"
+	provider "github.com/hashicorp/terraform-provider-tls/internal/provider"
 )
 
-func NewProvider() *schema.Provider {
-	p, _ := provider.New()
-	return p
+func NewProvider() framework.Provider {
+	return provider.New()
 }
