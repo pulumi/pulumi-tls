19c2189346fbf03c66741780d54362f294c9214c
Anton Tayanovskyy <anton@pulumi.com>
2023-02-13 17:46:22 -0500

    Upgrade to a pre-release version utilizing Terraform Plugin Framework (#170)
    Updates upstream to pre-release version 1a4077ae7e24.
    
    This subsumes updates to v4.0.0, v4.0.1, v4.0.2, v4.0.3, v4.0.4.
    
    The previous attempt [v4.7.0](https://github.com/pulumi/pulumi-tls/releases/tag/v4.7.0) to do this upgrade had to be pulled due to critical issue which state upgrades which is now fixed in github.com/pulumi/pulumi-terraform-bridge/pf v0.2.0.
    
    All the dependency upgrades:
    
    ```
    	github.com/hashicorp/terraform-plugin-framework v1.1.1
    	github.com/pulumi/pulumi-terraform-bridge/pf v0.2.0
    	github.com/pulumi/pulumi-terraform-bridge/v3 v3.40.0
    	github.com/pulumi/pulumi/sdk/v3 v3.53.1
    	github.com/pulumi/pulumi/pkg/v3 v3.53.1
    ```
    ---------
    
    Co-authored-by: Guinevere Saenger <guinevere@pulumi.com>

--- examples/self-signed-cert/go/main.go
+++ examples/self-signed-cert/go/main.go
@@ -17,7 +17,6 @@ func main() {
 		}
 
 		caRoot, err := tls.NewSelfSignedCert(ctx, "self-signed", &tls.SelfSignedCertArgs{
-			KeyAlgorithm:  caKey.Algorithm,
 			PrivateKeyPem: caKey.PrivateKeyPem,
 
 			ValidityPeriodHours: pulumi.Int(87600).ToIntOutput(),
@@ -44,7 +43,6 @@ func main() {
 		}
 
 		localCertReq, err := tls.NewCertRequest(ctx, "local", &tls.CertRequestArgs{
-			KeyAlgorithm:  certKey.Algorithm,
 			PrivateKeyPem: certKey.PrivateKeyPem,
 
 			Subject: tls.CertRequestSubjectArgs{
@@ -59,7 +57,6 @@ func main() {
 		localCert, err := tls.NewLocallySignedCert(ctx, "local", &tls.LocallySignedCertArgs{
 			CertRequestPem: localCertReq.CertRequestPem,
 
-			CaKeyAlgorithm:  caKey.Algorithm,
 			CaPrivateKeyPem: caKey.PrivateKeyPem,
 			CaCertPem:       caRoot.CertPem,
 
